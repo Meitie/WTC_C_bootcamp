f4040cf70b42eeda7f651f73e5d0708a35737ca6
Meitar S <44778545+Meitie@users.noreply.github.com>
2020-01-14 14:50:51 +0200

    uploading 4 and 5

---
+++ day05/ex05/ft_strupcase.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strupcase.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msegal <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/14 14:25:04 by msegal            #+#    #+#             */
+/*   Updated: 2020/01/14 14:49:56 by msegal           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strupcase(char *str)
+{
+	int i;
+	char *upper_case;
+
+	i = 0;
+	upper_case = str;
+	while (str[i] != '\0')
+	{
+		if (str[i] > 97 && str[i] < 122)
+		{
+			upper_case[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (upper_case);
+}
+
+int		main(void)
+{
+	char greetings[10] = "heLlO";
+	printf("%s", ft_strupcase(greetings));
+}
