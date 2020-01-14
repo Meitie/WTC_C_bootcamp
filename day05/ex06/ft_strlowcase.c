0444c1de1fe5def069c63d28acf2eec3a9c3583a
Meitar S <44778545+Meitie@users.noreply.github.com>
2020-01-14 16:15:18 +0200

    fixing 06

--- day05/ex06/ft_strlowcase.c
+++ day05/ex06/ft_strlowcase.c
@@ -21,7 +21,7 @@ char	*ft_strlowcase(char *str)
 	lower_case = str;
 	while (str[i] != '\0')
 	{
-		if (str[i] > 65 && str[i] < 90)
+		if (str[i] >= 65 && str[i] <= 90)
 			lower_case[i] = str[i] + 32;
 		i++;
 	}
