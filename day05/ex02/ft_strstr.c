fde47c0f16cd76673532ded7301d9eebc3b83970
Meitar S <44778545+Meitie@users.noreply.github.com>
2020-01-14 08:54:25 +0200

    new

--- day05/ex02/ft_strstr.c
+++ day05/ex02/ft_strstr.c
@@ -5,36 +5,37 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: msegal <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/01/13 14:25:39 by msegal            #+#    #+#             */
-/*   Updated: 2020/01/13 22:09:21 by msegal           ###   ########.fr       */
+/*   Created: 2020/01/14 08:48:16 by msegal            #+#    #+#             */
+/*   Updated: 2020/01/14 08:53:22 by msegal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-char	*ft_strstr(char *str, char *to_find)
+char *ft_strstr(char *str, char *to_find)
 {
 	int i;
-	int x;
+	int j;
 
-	i = 0;
- 	if (to_find == '\0')
+	if (!to_find)
 		return (str);
+	i = 0;
 	while (str[i] != '\0')
 	{
-		x = 0;
-		while (str[i + x] == to_find[x] && str[i + x] != '\0')
-		{	
-			if (to_find[x + 1] == '\0')
+		if (str[i] == to_find[0])
+		{
+			j = 1;
+			while (to_find[j] != '\0' && str[i + j] == to_find[j])
+				j++;
+			if (to_find[j] == '\0')
 				return (&str[i]);
-			x++;
 		}
 		i++;
 	}
 	return (0);
 }
 
-int		main(void)
+int		main()
 {
 	printf("%s", ft_strstr("hello there world", "there"));
 }
