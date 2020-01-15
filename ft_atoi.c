e6b85b544880cea8ae56ceabe2532e1d1e1e292a
Meitar S <44778545+Meitie@users.noreply.github.com>
2020-01-15 20:16:49 +0200

    upload atoi.c

---
+++ ft_atoi.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int ft_atoi(char *str)
+{
+    int i;
+    int nb;
+    int neg;
+
+    i = 0;
+    nb = 0;
+    neg = 1;
+    while (str[i] > 8 && str[i] < 17)
+        i++;
+    if (str[i] == '-' || str[i] == '+')
+    {
+        if (str[i] == '-')
+            neg = -1;
+        i++;
+    }
+    while (str[i] >= '0' && str[i] <= '9')
+    {
+        nb = (str[i] * 10) + (str[i] - '0');
+        i++;
+    }
+    return (nb * neg);
+}
+
+int main(void)
+{
+    char str[] = "51231";
+    printf("%d", ft_atoi(str));
+    return (0);
+}
