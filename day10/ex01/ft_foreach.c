1b0a299834a4c0720bdbd03b43fd94f0bd8874be
Meitar S <44778545+Meitie@users.noreply.github.com>
2020-01-21 09:12:25 +0200

    Add files via upload

--- day10/ex01/ft_foreach.c
+++ day10/ex01/ft_foreach.c
@@ -1,35 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*   ft_foreach.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: msegal <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/01/20 14:51:38 by msegal            #+#    #+#             */
-/*   Updated: 2020/01/20 17:18:09 by msegal           ###   ########.fr       */
+/*   Created: 2020/01/20 14:41:34 by msegal            #+#    #+#             */
+/*   Updated: 2020/01/20 15:49:17 by msegal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdlib.h>
 
-void	ft_putnbr(int nb)
+
+void	ft_foreach(int *tab, int length, void(*f)(int))
 {
-	long long i;
+	int i;
 
-	if (nb < 0)
-	{
-		write(1, "-", 1);
-		nb *= -1;
-	}
-	i = '0' + (nb % 10);
-	if (nb > 9)
-		ft_putnbr(nb / 10);
-	write(1, &i, 1);
-}
-
-int		main(int ac, char **av)
-{
-	if (ac == 2)
-		ft_putnbr((atoi(av[1])));
+	i = -1;
+	while (i++ < length)
+		(*f)(tab[i]);
 }
